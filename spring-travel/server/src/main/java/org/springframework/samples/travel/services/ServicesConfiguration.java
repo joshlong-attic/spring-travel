00c6f97ef13c0bea7ff60ff3529d073155787b1a
Josh Long <josh.long@springsource.com>
2011-07-06 01:06:34 -0700

    fixed a compiler error.

--- spring-travel/server/src/main/java/org/springframework/samples/travel/services/ServicesConfiguration.java
+++ spring-travel/server/src/main/java/org/springframework/samples/travel/services/ServicesConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.gemfire.GemfireCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
